e99503495fd8f5e7e9d465b8c3e9d91c811c2fb6
Gude Kiran Kumar <gude.1@campus.tu-berlin.de>
2019-10-02 22:01:31 +0200

    First commit.Saving image on character press s

--- src/student_interface.cpp
+++ src/student_interface.cpp
@@ -6,11 +6,39 @@
 namespace student {
 
  void loadImage(cv::Mat& img_out, const std::string& config_folder){  
-   throw std::logic_error( "STUDENT FUNCTION NOT IMPLEMENTED" );
+   throw std::logic_error( "STUDENT FUNCTION NOT IMPLEMENTED-2" );
  }
 
  void genericImageListener(const cv::Mat& img_in, std::string topic, const std::string& config_folder){
-    throw std::logic_error( "STUDENT FUNCTION NOT IMPLEMENTED" );
+    //throw std::logic_error( "STUDENT FUNCTION NOT IMPLEMENTED-ImageListener" );
+
+    static size_t id = 0;
+
+    const char *folder_path="/home/gkiri/Desktop/Computer_Vision/";
+
+    imshow(topic,img_in);
+
+    char key;
+    key = cv::waitKey(30);
+
+    std::cout<<"waitkey-"  << key << std::endl;
+
+    std::stringstream img_file;
+
+    if(key == 115 )
+    {
+      std::cout << "Team4:: Entered s"  << std::endl;
+      img_file << folder_path << std::setfill('0') 
+          << std::setw(3)  << (id++) << ".jpg";
+
+      //std::cout << img_file.str() << std::endl;
+      cv::imwrite( img_file.str(), img_in );
+
+      std::cout << "Saved image " << img_file.str() << std::endl;
+    }
+ 
+    std::cout << "Team4:: Exit"  << std::endl;
+       
   }
 
   bool extrinsicCalib(const cv::Mat& img_in, std::vector<cv::Point3f> object_points, const cv::Mat& camera_matrix, cv::Mat& rvec, cv::Mat& tvec, const std::string& config_folder){
