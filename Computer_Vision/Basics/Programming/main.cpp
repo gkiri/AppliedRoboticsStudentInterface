93e36df2344dd603a5e4ac25b0ed0b56c1960128
Gude Kiran Kumar <gude.1@campus.tu-berlin.de>
2019-10-03 20:55:10 +0200

    Images -calibration

---
+++ Computer_Vision/Basics/Programming/main.cpp
@@ -0,0 +1,19 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+
+ if( argc != 2)
+    {
+     cout <<" Usage: display_image ImageToLoadAndDisplay" << endl;
+     return -1;
+    }
+
+return 0;
+}
+
