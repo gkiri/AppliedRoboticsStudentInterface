dddce8cf2e9a79b1c69fee95cc5d9a7ec8ab4223
Gude Kiran Kumar <gude.1@campus.tu-berlin.de>
2020-02-13 08:51:05 +0100

    code commenting and findrobot update

--- include/collision_detection.hpp
+++ include/collision_detection.hpp
@@ -17,20 +17,83 @@
 #include "Utils.hpp"
 
 
+/**
+ * line line Intersection  detection
+ *  * 
+ * @param A - start point of line1
+ * @param B - end point of line1
+ * @param C - start point of line2
+ * @param D - end point of line2
+ * 
+ * @output - return the TRUE = Collision happened ,FALSE = No collision
+*/
 bool linelineIntersection(Point A,Point B,Point C,Point D,Point *X);
+
+
+/**
+ * line circle Intersection  detection
+ *  * 
+ * @param r - radius
+ * @param a - line coeeficients a
+ * @param b - line coeeficients b
+ * @param c - line coeeficients c
+ * 
+ * @output - return the TRUE = Collision happened ,FALSE = No collision
+*/
 int linecircleIntersection(double r, double a , double b, double c ,std::vector<Point>& X);
 //bool lineArcIntersection(Point Line_Start,Point Line_End,Point Arc_Start,Point Arc_End,double r,Point center);
+
+
+/**
+ * line ARC Intersection  detection
+ *  * 
+ * @param line - line
+ * @param arc - arc data
+ * 
+ * @output - return the TRUE = Collision happened ,FALSE = No collision
+*/
 bool lineArcIntersection(struct arc_extract line,struct arc_extract arc);
+
+
+/**
+ * line arc Intersection  detection
+ *  * 
+ * @param line - line
+ * @param arc - arc
+ * @param intersect_points - intersect_points
+ * @output - return the TRUE = Collision happened ,FALSE = No collision
+*/
 bool lineArcIntersection_prof(struct arc_extract line,struct arc_extract arc,std::vector<Point>& intersect_points);
 bool  Construct_Bounding_Box(Polygon& input , Polygon& output);
 void construct_line_structure(arc_extract& line_data,Point Arc_Start ,Point Arc_End);
 int get_line_circle_intersection(Point linestart, Point lineend,Point center, double radius,Point& firstIntersection,Point& secondIntersection);
+
+
+/**
+ * is_line_colliding_circle
+ *  * 
+ * @param linestart - start point of line
+ * @param lineend - end point of line
+ * @param center - center
+ * @param radius - radius of circle
+ * 
+ * @output - return the TRUE = Collision happened ,FALSE = No collision
+*/
 bool is_line_colliding_circle(Point linestart, Point lineend, Point center, double radius);
+
+/**
+ * Detect pointlies inside given polygons
+ *  * 
+ * @param cv_poly_list - obstacle_list
+ * @param pt - point
+ * 
+ * @output - return the TRUE = Collision happened ,FALSE = No collision
+*/
 bool Detect_point_liesin_polygon(Point pt,std::vector<Polygon> cv_poly_list);
 
 
 /**
- * DUbins 3 segment check with globally using narrow and broad phase
+ * Dubins 3 segment check with globally using narrow and broad phase
  * 
  * @param obstacle_list - obstacle_list
  * @param segment - 3 segments
